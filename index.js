10f36da8a70c8123c8cfd646931c1fde506b8a17
ioanbutiu <ioan.butiu@gmail.com>
2022-05-11 19:05:24 -0400

    added sample data

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./projectBank");
+
 const app = express();
 const port = 3000;
 
+app.use(morgan("dev"));
+// app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Wizard News</title>
+    </head>
+    <body>
+      <ul>
+        ${posts.map((post) => `<li>${post.name}</li>`).join(" ")}
+      </ul>
+    </body>
+  </html>`;
+
+  res.send(html);
 });
 
 app.listen(port, () => {
